32ccfa9de3265cfe6b5f54546262106f5f751edf
NIKHIL9676 <75869382+NIKHIL9676@users.noreply.github.com>
2022-10-20 19:35:44 +0530

    Update Players.js

--- src/Players.js
+++ src/Players.js
@@ -13,7 +13,7 @@ const PlayersList=[
     {id:1,name:"Virat Kohli",country:"India",category:"RightHand Batsman",image:'https://cdn.dnaindia.com/sites/default/files/styles/full/public/2021/10/22/1002052-virat-kohli.jpg'},
     {id:2,name:"Glenn Maxwell",country:"Australia",category:"LeftHand Batsman",image:'https://i.guim.co.uk/img/media/ed99d6654ae81fcce3d1d7fc921fd562d34e6d5c/0_51_4503_2702/master/4503.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=28edcc0820aaff16a87de2f86ff019c5'},
     {id:3,name:"KL Rahul",country:"India",category:"RightHand Batsman",image:'https://images.indianexpress.com/2021/09/kl-rahul-1-2.jpg'},
-    {id:4,name:"Rohit Sharma",country:"India",category:"RightHand Batsman",https://d3q33rbmdkxzj.cloudfront.net/1589466137456_aFz3CE.png'}
+    {id:4,name:"Rohit Sharma",country:"India",category:"RightHand Batsman",'https://d3q33rbmdkxzj.cloudfront.net/1589466137456_aFz3CE.png'}
 ];
 const players=PlayersList.map(plr=>(
     <Card>
