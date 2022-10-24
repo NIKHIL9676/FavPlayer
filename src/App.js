ffafd94569566461e16c26f7851c486c2b35acc2
NIKHIL9676 <75869382+NIKHIL9676@users.noreply.github.com>
2022-10-24 11:57:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import NavBar from './Header';
 import Counter from './Counter';
 import Players from './Players'
+import Footer from './Footer';
 export default function App() {
   return (
     <div>
   <Counter/>
      <NavBar/>
     <Players/> 
+    <Footer/>
     </div>  );
 }
